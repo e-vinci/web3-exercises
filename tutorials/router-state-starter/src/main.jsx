5050fc83c9821dd7e0650ee6b1ca88fb35a2e5ba
e-baron <raphael.baroni@vinci.be>
2024-10-16 18:19:01 +0200

    new: react router state starter and tutorial

--- tutorials/router-state-starter/src/main.jsx
+++ tutorials/router-state-starter/src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "components/App/App";
+import HomePage from "components/pages/HomePage";
+import AddPizzaPage from "components/pages/AddPizzaPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "add-pizza",
+        element: <AddPizzaPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
