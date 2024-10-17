2e96497fa7af9c128c8895062e6fd422dc1fd841
e-baron <raphael.baroni@vinci.be>
2024-10-17 12:21:15 +0200

    new: module7 router state tutorials

---
+++ tutorials/router-starter/src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import CinemaPage from "components/Pages/CinemaPage";
+import HomePage from "components/Pages/HomePage";
+import MovieListPage from "components/Pages/MovieListPage";
+
+const App = () => {
+  return (
+    <>
+      <CinemaPage />
+      <MovieListPage />
+      <HomePage />
+     </>
+  );
+};
+
+export default App;
