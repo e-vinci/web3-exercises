8d39ad93bd92f3e7799a8725df06193124bf423c
Baroni Raphaël <raphael.baroni@vinci.be>
2024-10-23 09:20:03 +0200

    module7 : tutos and exercises (#9)
    * new: module7 router state tutorials
    
    * new: module7 ex7.1
    
    * new: module7 ex7.2
    
    * new: module7 ex7.3
    
    * new: module7 ex7.4
    
    * update: module7 following review comments

---
+++ tutorials/router-state-starter/src/components/pages/HomePage.jsx
@@ -0,0 +1,29 @@
+import AudioPlayer from "components/AudioPlayer/AudioPlayer";
+import sound from "../../assets/sounds/Infecticide-11-Pizza-Spinoza.mp3";
+import PizzaMenu from "components/PizzaMenu/PizzaMenu";
+import DrinkMenu from "components/DrinkMenu/DrinkMenu";
+
+const HomePage = () => {
+  // TODO : Get state & functions
+
+  return (
+    <>
+      <h1>Ma Pizzeria</h1>
+      <p>
+        Parce que nous aimons le JS/TS, vous pouvez cliquer sur le header pour
+        démarrer / stopper la musique ; )
+      </p>
+      <AudioPlayer
+        sound={sound}
+        actionToBePerformed={actionToBePerformed}
+        clearActionToBePerformed={clearActionToBePerformed}
+      />
+
+      <PizzaMenu pizzas={pizzas} />
+
+      <DrinkMenu title="Nos boissons" drinks={drinks} />
+    </>
+  );
+};
+
+export default HomePage;
